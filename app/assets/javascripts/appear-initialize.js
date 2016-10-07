fbffd2427f12e7a49a13424d78d53d9f90271208
Bobolay <bogdan4uk@gmail.com>
2016-10-07 17:33:57 +0300

    generals change of this page

--- app/assets/javascripts/appear-initialize.js
+++ app/assets/javascripts/appear-initialize.js
@@ -1,14 +1,13 @@
-is_touch_screen = 'ontouchstart' in document.documentElement;
-width = window.innerWidth;
-
-if(is_touch_screen || width < 640) {
-    console.log("it works")
-} else {
-	$(function() {
-	  var $appeared = $('.about-us .text, .about-us .text-middle, .our-kitchen .text, .dish-frame, .inner-text-right, .line-left, .chef-photo, .price-right, .price-left, .left-to-right, .right-to-left');
-	  $appeared.appear();
-	  $(document.body).on('appear', function(e, $affected) {
-	    $affected.addClass('appeared')
-	  });
-	});
-}
+$(document).on('ready', function(){
+  if(is_touch_screen || width < 640) {
+      console.log("it works")
+  } else {
+    $(function() {
+      var $appeared = $('.animated-text-1, .animated-text-2, .animated-text-3, .animated-text-4, .animated-text-5');
+      $appeared.appear();
+      $(document.body).on('appear', function(e, $affected) {
+        $affected.addClass('appeared')
+      });
+    });
+  };
+});
--- app/assets/javascripts/main.coffee
+++ app/assets/javascripts/main.coffee
@@ -1,6 +1,7 @@
-$("a[href^='#']").on "click", (e)->
-  e.preventDefault()
-  $link = $(this)
-  href = $link.attr("href")
-  target_top = $(href).offset().top
-  $("body").animate({scrollTop: target_top})
+$(document).on "ready", ->  
+  $("a[href^='#']").on "click", (e)->
+    e.preventDefault()
+    $link = $(this)
+    href = $link.attr("href")
+    target_top = $(href).offset().top
+    $("body").animate({scrollTop: target_top})
