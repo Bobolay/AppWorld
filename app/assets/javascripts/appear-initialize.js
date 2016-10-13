1415fb144a67f9259b1b625faeb54196dd7fd83f
Bobolay <bogdan4uk@gmail.com>
2016-10-13 12:41:39 +0300

    added video

--- app/assets/javascripts/appear-initialize.js
+++ app/assets/javascripts/appear-initialize.js
@@ -1,9 +1,26 @@
 $(document).on('ready', function(){
-  if(is_touch_screen || width < 640) {
-      // console.log("it works")
+
+  var is_touch_screen = 'ontouchstart' in window
+  var width = $(window).width()
+
+  if(is_touch_screen || width < 1200) {
+    console.log("U are using touch device with screen under 1200px !");
   } else {
+
+    // A N I M AT E     T E X T     O N     B A N N E R
+
+    setTimeout(function(){
+      $('.animated-text-1').addClass('appeared')
+    }, 250);
+
+    setTimeout(function(){
+      $('.animated-text-2').addClass('appeared')
+    }, 750);
+    
+    // A N I MA T E     A N O T H E R     T E X T
+
     $(function() {
-      var $appeared = $('.animated-text-1, .animated-text-2, .animated-text-3, .animated-text-4, .animated-text-5');
+      var $appeared = $('.animated-text-3, .animated-text-4, .animated-text-5');
       $appeared.appear();
       $(document.body).on('appear', function(e, $affected) {
         $affected.addClass('appeared')
